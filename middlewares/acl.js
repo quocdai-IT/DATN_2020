89a3492310cf68a5c11508ef94b27d18408e7477
dai-nguyen207 <dainguyen@airme.vn>
2020-08-24 20:43:07 +0700

    fix bug

--- middlewares/acl.js
+++ middlewares/acl.js
@@ -4,11 +4,12 @@ const rules = {
 };
 
 const ACL = [
+  { method: "post", path: "/xemthi", rules: [rules.KT] },
   { method: "get", path: "/xemthi", rules: [rules.KT] },
   { method: "get", path: "/xemthi/public", rules: [rules.KT] },
   { method: "get", path: "/draf", rules: [rules.GV, rules.KT] },
-  {method : "post", path: "/draf", rules: [rules.GV]},
-  {method: "get", path: '/xemthi/accept', rules: [rules.KT]}
+  { method: "post", path: "/draf", rules: [rules.GV] },
+  { method: "get", path: "/xemthi/accept", rules: [rules.KT] },
 ];
 
 module.exports = ACL;
